bcae2344247cfee0c97e79c21cf224e10bcf32c0
ylchong <mpechon@nus.edu.sg>
2019-01-10 15:31:47 +0800

    added lib dependency instructions for calibration

---
+++ imu/dependencies_instructions.py
@@ -0,0 +1,5 @@
+1. sudo apt-get install libi2c-dev libeigen3-dev libboost-program-options-dev
+2. git clone https://github.com/DavidEGrayson/minimu9-ahrs.git
+3. cd minimu9-ahrs & make
+4. sudo make install
+
