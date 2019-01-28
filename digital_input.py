5475bc76a1a9571818c3a08d64cdbac34c19c26b
ylchong <mpechon@nus.edu.sg>
2019-01-28 20:31:07 +0800

    removed old commented code

--- digital_input.py
+++ digital_input.py
@@ -7,14 +7,7 @@ GPIO_TRIGGER = 21
 
 GPIO.setup(GPIO_TRIGGER, GPIO.IN) 
 
-#GPIO.output(GPIO_TRIGGER, False)
 while True:
     print (GPIO.input(GPIO_TRIGGER))
-
-#GPIO.output(GPIO_TRIGGER, True) 
-#while True:
-#	GPIO.output(GPIO_TRIGGER, True) 
-#	time.sleep(1)
-#	print("setting to high")
 
 GPIO.cleanup()
