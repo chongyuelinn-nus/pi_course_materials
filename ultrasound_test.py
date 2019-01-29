9de5e907643678be2c4a7a5608dd0da4a3f5635f
ylchong <mpechon@nus.edu.sg>
2019-01-29 17:37:18 +0800

    changed gpio numbers

--- ultrasound_test.py
+++ ultrasound_test.py
@@ -6,8 +6,8 @@ import time
 GPIO.setmode(GPIO.BCM)
  
 #set GPIO Pins
-GPIO_TRIGGER = 23
-GPIO_ECHO = 24
+GPIO_TRIGGER = 20
+GPIO_ECHO = 21
  
 #set GPIO direction (IN / OUT)
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
