18179960c709efcdf05dd972c07ae1368a400ea3
mpechon@nus.edu.sg <mpechon@nus.edu.sg>
2020-03-03 16:38:37 +0800

    finally effing working potentiometer

--- adc.py
+++ adc.py
@@ -1,22 +1,18 @@
 import board
 import busio
 import time
-import matplotlib.pyplot as plt
+print ("starting up... this may take awhile...")
 i2c = busio.I2C(board.SCL, board.SDA)
 
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
 ads = ADS.ADS1015(i2c, gain=2/3)
-#chan = AnalogIn(ads, ADS.P0)
 chan = AnalogIn(ads, ADS.P0, ADS.P1)
  
-#time.sleep(1)
 t = ()
 val = ()
 for i in range(0,1000):
     t = t + (i,)
-    val = val + (chan.voltage,)
-    #time.sleep(0.01)
-plt.plot(t,val,'r--')
-plt.show()
+    print(chan.voltage)
+    time.sleep(0.3)
